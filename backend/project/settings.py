4c8e90e23fa45fa26b6fed0c731712db71d15619
Sora <sky.roofcommon.0314@gmail.com>
2025-05-06 00:56:59 +0900

    add django-environ and modify setting.py, .env and requirements.txt

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -15,10 +15,11 @@ from pathlib import Path
 import environ
 import os
 
-env = environ.Env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
